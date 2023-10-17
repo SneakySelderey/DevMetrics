bbfbf69d1357dd929f47d72c8dc32741104eb4c2
Alexader Ershov <ershovsanek.centr@yandex.ru>
2023-10-17 18:55:13 +0300

    feat: implement WebView to show data

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,17 @@ interface IDictionary {
     [key: string]: number;
 }
 
+function getSummarizedMetrics(docs: IDictionary)
+{
+	let lineCountDelta = 0;
+	for (let key in docs)
+	{
+		console.log(key + " " + docs[key]);
+		lineCountDelta += docs[key];
+	}
+	return lineCountDelta;
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,26 +26,43 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "devmetrics" is now active!');
 
-	let lineCountDelta: number = 0;
 	let currentDocNewLineCount: number = 0;
 	const docs: IDictionary = {};
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devmetrics.showLines', () => {
+	let disposable = vscode.commands.registerCommand('devmetrics.showData', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 
-		lineCountDelta = 0;
-		for (let key in docs)
-		{
-			console.log(key + " " + docs[key]);
-			lineCountDelta += docs[key];
-		}
+		let linesCount = getSummarizedMetrics(docs);
 
-		vscode.window.showInformationMessage('Total amount of lines written: ' + lineCountDelta);
+		const panel = vscode.window.createWebviewPanel(
+			'devMetrics', // Identifies the type of the webview.
+			'DevMetrics', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+			{} // Webview options.
+		  );
+
+		  panel.webview.html = getWebviewContent(linesCount);
+
+		  const updateWebview = () => {
+
+			panel.webview.html = getWebviewContent(getSummarizedMetrics(docs));
+		  };
+	
+		  // Set initial content
+		  updateWebview();
+	
+		  // And schedule updates to the content every second
+		  setInterval(updateWebview, 1000);
 	});
+
+	// if (vscode.window.activeTextEditor?.document.fileName !== undefined && vscode.window.activeTextEditor?.document.lineCount !== undefined)
+	// 	{
+	// 		docs[vscode.window.activeTextEditor?.document.fileName as string] = vscode.window.activeTextEditor?.document.lineCount as number;
+	// 	}
 
 	vscode.window.onDidChangeActiveTextEditor(function(event) {
 		if (event?.document.fileName !== undefined && vscode.window.activeTextEditor?.document.lineCount !== undefined)
@@ -53,3 +81,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(linesCount: number) {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>DevMetrics data</title>
+  </head>
+  <body>
+	  <h1>Lines of code written during current session: ${linesCount}</h1>
+  </body>
+  </html>`;
+  }
