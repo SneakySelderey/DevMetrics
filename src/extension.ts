d6f4484633102a5012fa3179af4e84d8b9347642
Alexander Ershov <ershovsanek.centr@yandex.ru>
2023-10-20 22:15:12 +0300

    feat: data reset command added

--- src/extension.ts
+++ src/extension.ts
@@ -113,7 +113,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('devmetrics.showData', () => {
+	let showDataCommand = vscode.commands.registerCommand('devmetrics.showData', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 
@@ -143,6 +143,12 @@ export function activate(context: vscode.ExtensionContext) {
 		  setInterval(updateWebview, 1000);
 	});
 
+	let resetDataCommand = vscode.commands.registerCommand('devmetrics.resetData', () => 
+	{
+		context.globalState.update("metrics", undefined);
+		metrics = new Metrics();
+	});
+
 	// this block of code tracks the initial document if it was already opened on startup
 	if (vscode.window.activeTextEditor?.document.fileName !== undefined 
 		&& vscode.window.activeTextEditor?.document.lineCount !== undefined 
@@ -163,7 +169,8 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(showDataCommand);
+	context.subscriptions.push(resetDataCommand);
 }
 
 // This method is called when your extension is deactivated
